9aa28effb3c90a75857661c06871c267e17e88d2
Newms34 <Newms3450@msn.com>
2016-07-16 12:26:21 -0400

    another try

--- test/index.js
+++ test/index.js
@@ -1,59 +1,92 @@
 var chalk = require('chalk'),
     gp = require('get-pixels'),
     imSz = require('image-size');
-var dims = imSz('river.jpg');
-gp('river.jpg', function(err, px) {
+var dims = imSz('doghed.png');
+gp('doghed.png', function(err, px) {
     if (err) {
         console.log(chalk.red('OH NO!'), 'Error:', err);
-        px.get(x, y, 0)
+        px.get(x, y, 0);
         return;
     }
     console.log(px.data);
-    var rgbObj = {};
+    var rgbArr = [];
     for (var y = 0; y < dims.height; y++) {
+        var row = [];
         for (var x = 0; x < dims.width; x++) {
             var newRGB = {
                 red: px.get(x, y, 0),
                 green: px.get(x, y, 1),
                 blue: px.get(x, y, 2)
             };
-            rgbObj[x + '-' + y] = newRGB;
+            row.push(newRGB);
         }
+        rgbArr.push(row);
     }
-    [
-        [],
-        []
-    ]
     //we now have an array of all pixel rgb vals, with each pixel ALSO assigned its x and y coords
-    console.log(rgbObj)
+    console.log(rgbArr)
     var imDiv = Math.ceil(dims.width / 80); //get the number of pixels wide each pixel 'chunk' will be
     var chunkArr = [];
     var finalColArr = []; //to hold the final colors for chalk
-    for (var h = 0; h < dims.height; h += imDiv) {
-        for (var w = 0; w < dims.width; w += imDiv) {
-            console.log(h, '-', w);
-            var r = 0,
-                g = 0,
-                b = 0,
-                t = Math.pow(imDiv, 2);
-            for (var a = 0; a < imDiv; a++) {
-                for (var p = 0; p < imDiv; p++) {
-                    if (rgbObj[(h + a) + '-' + (w + p)]) {
-                        r += rgbObj[(h + a) + '-' + (w + p)].red;
-                        g += rgbObj[(h + a) + '-' + (w + p)].green;
-                        b += rgbObj[(h + a) + '-' + (w + p)].blue;
+    for (var i = 0; i < dims.height; i += imDiv) {
+        console.log('on row', i, 'imDiv', imDiv);
+        for (var j = 0; j < dims.width; j += imDiv) {
+            console.log('on col', j)
+            var tot = Math.pow(imDiv, 2);
+            var avg = {
+                    r: 0,
+                    g: 0,
+                    b: 0
+                }
+                //now we're in one 'chunk'. We average all the rgb vals, and...
+            for (var x = 0; x < imDiv; x++) {
+                for (var y = 0; y < imDiv; y++) {
+                    var theX = j + x;
+                    var theY = i + y;
+                    if (rgbArr[theY] && rgbArr[theY][theX]) {
+                        avg.r += rgbArr[theY][theX].red;
+                        avg.g += rgbArr[theY][theX].green;
+                        avg.b += rgbArr[theY][theX].blue;
                     } else {
-                        t--;
+                        //this px doesnt exist, so subtract from tot (so that our average is accurate)
+                        tot--;
                     }
                 }
             }
-            chunkArr.push({
-                r: r / t,
-                g: g / t,
-                b: b / t
-            });
+            //now average
+            avg.r = avg.r / tot;
+            avg.g = avg.g / tot;
+            avg.b = avg.b / tot;
+            // console.log(avg.g)
+            //and push into chunkarr
+            chunkArr.push(avg);
         }
+
     }
+    console.log(chunkArr)
+    // for (var h = 0; h < dims.height; h += imDiv) {
+    //     for (var w = 0; w < dims.width; w += imDiv) {
+    //         var r = 0,
+    //             g = 0,
+    //             b = 0,
+    //             t = Math.pow(imDiv, 2);
+    //         for (var a = 0; a < imDiv; a++) {
+    //             for (var p = 0; p < imDiv; p++) {
+    //                 if (rgbArr[(h + a) + '-' + (w + p)]) {
+    //                     r += rgbArr[(h + a) + '-' + (w + p)].red;
+    //                     g += rgbArr[(h + a) + '-' + (w + p)].green;
+    //                     b += rgbArr[(h + a) + '-' + (w + p)].blue;
+    //                 } else {
+    //                     t--;
+    //                 }
+    //             }
+    //         }
+    //         chunkArr.push({
+    //             r: r / t,
+    //             g: g / t,
+    //             b: b / t
+    //         });
+    //     }
+    // }
 
     for (var n = 0; n < chunkArr.length; n++) {
         if (!isNaN(chunkArr[n].r) && !isNaN(chunkArr[n].g) && !isNaN(chunkArr[n].b)) {
