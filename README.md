9a78672fb6f1ae04556a97797d4a144772962dba
Newms34 <newms3450@gmail.com>
2019-10-25 22:00:47 -0400

    update! pictures now should render correctly and not be 'skewed'.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Chalk Image Converter
 *Important note!:*
-The format of the options available for chalk-image has recently changed (they're actually easier now! You're welcome). Please make sure you read the new options format if your app breaks!
+The format of the options available for chalk-image has changed (they're actually easier now! You're welcome). Please make sure you read the new options format if your app breaks!
 
 ---
 ## About:
@@ -28,12 +28,13 @@ Just include your chalk-image: `const ci = require('chalk-image');`
 
 Then draw!: `ci.drawImg('path/to/my/img.jpg');`
 
-Works best with BMPs and JPEGs!
+Or export a promise!: `ci.drawImage('path/to/my/img.jpg',{exportMe:true}).then(...//do magic)`
+Works best with BMPs and JP(E)Gs!
 
 ---
 ### Options:
 There are a number of options available. These are passed to `.drawImg()` as a secondary optional options object. The properties of this object are as follows:
- - **exportMe**: Default false. If true, `.drawImg()` will return a `.then`able promise. Otherwise, `.drawImg()` will simply draw the image when it's finished mathing it. *NOTE:* Even without a promise, `.drawImg()`, will *not* simply return the image string. This is because the drawing process involves some asynchronous wizardry.
+ - **exportMe**: Default false. If true, `.drawImg()` will return a `.then`able promise. Otherwise, `.drawImg()` will simply draw the image when it's finished mathing it. *NOTE:* Even without a promise, `.drawImg()`, will *not* simply return the image string (so don't try doing `const myImage = chalkImage.drawImg('somefile)`. It won't work). This is because the drawing process involves some asynchronous wizardry.
  - **verbose**: Default false. If true, you'll get some extra info about the image (and console) dimensions. Useful if you wanna debug stuff.
  - **colMode**: Default no value. If set to `16` or `16m`, will draw in 16-million color mode. If set to `256`, will draw in 256-color mode. Otherwise, It'll draw in good-old-fashioned 16 color mode. 
 
@@ -44,8 +45,9 @@ Go ahead and `cd` into the 'tests' folder. I've stuck a separate readme in there
 ---
 ### Issues:
  1. Does not currently work with animated GIFs. This for a number of reasons. Firstly, GIFs are not necessarily stored in individual, separate frames. If part of an animated GIF doesn't change, that part may not necessarily be 'part' of each new frame. Second, the amount of data for animated gifs is actually rather *huge* (a 100x100, 3 second GIF, with 256 colors at 30FPS, is potentially 2.3 *billion* separate numbers)! Finally, while there are methods to clear individual pixels in the console, none of them are terribly quick or efficient.
- 2. There's a bit of an issue with certain combinations of image dimensions and console dimensions, where the resulting image 'rows' end up overflowing. This in turn results in heavily distorted images. If you can help fix it, feel free to submit a pull request.
- 3. Full color *should* work on all systems. However, until recently, Windows systems (or ChalkJS?) didn't support color. So be warned that YMMV.
+ 2. ~~There's a bit of an issue with certain combinations of image dimensions and console dimensions, where the resulting image 'rows' end up overflowing. This in turn results in heavily distorted images. If you can help fix it, feel free to submit a pull request.~~ Nevermind, this should be fixed!
+ 3. Full color *should* work on all systems. However, until recently, Windows systems (or ChalkJS?) didn't support color. Be warned that YMMV.
+ 4. Why Kurt Cobain? Because I searched for "high-contrast image" in Google, and that was the first picture that came up. So there.
 
 ---
 ### Credits:
--- index.js
+++ index.js
@@ -13,15 +13,15 @@ const vc = (t, m) => {
     }
 }
 
-const drawImg = (im_url,opts) => {
+const drawImg = (im_url, opts) => {
     // im_url, colMode, exportMe
     //if mode is blank or not one of the defined options, render using color 'names' (i.e., red, green, yellow, etc.). 
     //if 256, render using 256 color mode. if 16 or 16m, use 16million color mode
     // console.log(im_url, colMode, exportMe)
     let exportMe = !!opts && !!opts.exportMe,
         def = null;
-        colOpts = ['256','16m','16'],
-        colMode = !!opts && !!opts.colMode && colOpts.includes(opts.colMode)?opts.colMode:null,
+    colOpts = ['256', '16m', '16'],
+        colMode = !!opts && !!opts.colMode && colOpts.includes(opts.colMode) ? opts.colMode : null,
         vb = !!opts && !!opts.verbose;
     try {
         var dims = imSz(im_url);
@@ -33,14 +33,15 @@ const drawImg = (im_url,opts) => {
 
     const limitCols = (colMode != '16' && colMode != '16m' && colMode != '256');
     if (exportMe) {
-        vc(['Creating promise!'],true)
+        vc(['Creating promise!'], true)
         def = Q.defer();
     }
-    gp(im_url, function(err, px) {
-        vc(['image width', dims.width, 'which is columns (cmd width)', process.stdout.columns, '.\nImage height', dims.height, 'which is rows (cmd height)', process.stdout.rows], vb)
-        const imDivWd = Math.max(Math.floor(dims.width / (process.stdout.columns)), 1), //get the number of pixels wide each pixel 'chunk' will be
-        imDivHt = imDivWd*2;
-            // imDivHt = Math.max(Math.floor(dims.height / (process.stdout.rows)), 1);
+    gp(im_url, function (err, px) {
+        vc(['img width', dims.width, '\nimg height', dims.height, '\nTerminal width', process.stdout.columns, '\nTerminal Height', process.stdout.rows], vb)
+        // vc(['image width', dims.width, 'which is columns (cmd width)', process.stdout.columns, '.\nImage height', dims.height, 'which is rows (cmd height)', process.stdout.rows], vb)
+        const imDivWd = Math.max(Math.ceil(dims.width / (process.stdout.columns)), 1),//get the number of pixels wide each pixel 'chunk' will be
+            imDivHt = imDivWd * 2;
+        // imDivHt = Math.max(Math.floor(dims.height / (process.stdout.rows)), 1);
         vc(['chunk width', imDivWd, 'px by height', imDivHt, 'px'], vb)
         if (err) {
             console.log(chalk.red('OH NO!'), 'Error:', err);
@@ -49,164 +50,171 @@ const drawImg = (im_url,opts) => {
         const rgbArr = [];
 
         //get all pixel RGB vals
+        //note that we ignore every alpha (transparency) val, as this is unusable for the terminal
         for (let y = 0; y < dims.height; y++) {
             let row = [];
             for (let x = 0; x < dims.width; x++) {
                 let newRGB = {
-                    red: px.get(x, y, 0),
-                    green: px.get(x, y, 1),
-                    blue: px.get(x, y, 2)
+                    r: px.get(x, y, 0),
+                    g: px.get(x, y, 1),
+                    b: px.get(x, y, 2)
                 };
                 row.push(newRGB);
             }
             rgbArr.push(row);
         }
+        // console.log(rgbArr)
         //we now have an array of all pixel rgb vals, with each pixel ALSO assigned its x and y coords (based on where it is in the array)
-        const chunkArr = [];
+        //now we need to construct each chunk
+        let chunkArr = [];
         const finalColArr = []; //to hold the final colors for chalk
         vc(['pixels in first chunk', imDivWd * imDivHt], vb)
-        let cx = 0,
-            cy = 0,
+        let cx = 0,//'center' x
+            cy = 0,//'center' y
             dx = imDivWd, //the width of the chunk
             dy = imDivHt, //the height of the chunk
             chunkX = 0,
             chunkY = 0,
             theX = 0,
-            theY = 0;
-        //[y] THEN [x]
-        for (cy = 0; cy < dims.height-imDivHt; cy += imDivHt) {
-            for (cx = 0; cx < dims.width-imDivWd; cx += imDivWd) {
-                let tot = dx * dy, //num px in each chunk
-                    avg = {
-                        r: 0,
-                        g: 0,
-                        b: 0,
-                        sx: cx,
-                        sy: cy,
-                    };
-                for (chunkX = 0; chunkX < dx; chunkX++) {
-                    for (chunkY = 0; chunkY < dy; chunkY++) {
-                        theX = chunkX + cx;
-                        theY = chunkY + cy;
-                        if (rgbArr[theY] && rgbArr[theY][theX]) {
-                            avg.r += rgbArr[theY][theX].red;
-                            avg.g += rgbArr[theY][theX].green;
-                            avg.b += rgbArr[theY][theX].blue;
-                        } else {
-                            //this px doesnt exist, so subtract from tot (so that our average is accurate)
-                            tot--;
+            theY = 0,
+            str = '',
+            badPxs = 0;
+            //[y] THEN [x]
+
+            const chunkSize = {
+                w: Math.ceil(dims.width / process.stdout.columns),
+                h: Math.ceil(dims.width / process.stdout.columns) * 2
+            },
+                defaultChunkSize = chunkSize.w * chunkSize.h,//assuming we're not on an edge or something (where we may be missing pixels), this should be the total number of pixels in each chunk
+                needsBuffer = Math.max(0, process.stdout.columns - dims.width);
+            let chnkCounter = 0;
+            const numChunks = {
+                w: Math.floor(process.stdout.columns / chunkSize.w) * chunkSize.w,
+                h: Math.floor(dims.height / chunkSize.h)
+            };
+            // console.log('CHUNKS NOW', chunkSize, 'IMG SIZE', dims, 'CONSOLE WIDTH', process.stdout.columns, 'PIXELS', rgbArr.length * rgbArr[0].length, 'NUMBER O CHUNKZ', numChunks)
+
+            for (let cy = 0; cy < numChunks.h; cy++) {
+                for (let cx = 0; cx < numChunks.w; cx++) {
+                    let tot = defaultChunkSize,
+                        sumPx = { r: 0, g: 0, b: 0, t: defaultChunkSize };
+                    for (let cix = 0; cix < chunkSize.w; cix++) {
+                        for (let ciy = 0; ciy < chunkSize.h; ciy++) {
+                            let actualX = (cx * chunkSize.w) + cix,
+                                actualY = (cy * chunkSize.h) + ciy;
+                            if (!!rgbArr[actualY] && !!rgbArr[actualY][actualX]) {
+                                sumPx.r += rgbArr[actualY][actualX].r;
+                                sumPx.g += rgbArr[actualY][actualX].g;
+                                sumPx.b += rgbArr[actualY][actualX].b;
+                            } else {
+                                // tot--;
+                                sumPx.t--;
+                            }
                         }
                     }
-                }
-                //finally, average the pixels
-                avg.r = avg.r / tot;
-                avg.g = avg.g / tot;
-                avg.b = avg.b / tot;
-                //and push into chunkarr
-                chunkArr.push(avg);
+                    sumPx.r = sumPx.r / tot;
+                    sumPx.g = sumPx.g / tot;
+                    sumPx.b = sumPx.b / tot;
+                    sumPx.cx = cx;
+                    sumPx.cy = cy;
+                    sumPx.c = chnkCounter;
+                    chnkCounter++;
+                    // || (needsBuffer && chnkCounter === dims.width - 1)
+                    if ((chnkCounter === numChunks.w)) {
+                        //at end of row
+                        chnkCounter = 0;
+                        // if(!! needsBuffer){
 
-            }
-        }
-        // require('fs').writeFileSync('out.json',JSON.stringify(chunkArr),'utf-8')
-        // for (let i = 0; i < dims.height; i += imDivHt) {
-        //     for (let j = 0; j < dims.width; j += imDiv) {
-        //         let tot = imDiv * imDivHt;//total number of pixels in each chunk
-        //         let avg = {
-        //             r: 0,
-        //             g: 0,
-        //             b: 0
-        //         }
-        //         //now we're in one 'chunk'. We average all the rgb vals
-        //         for (let y = 0; y < imDivHt; y++) {
-        //             for (let x = 0; x < imDiv; x++) {
-        //                 let theX = j + x;
-        //                 let theY = i + y;
-        //                 if (rgbArr[theY] && rgbArr[theY][theX]) {
-        //                     avg.r += rgbArr[theY][theX].red;
-        //                     avg.g += rgbArr[theY][theX].green;
-        //                     avg.b += rgbArr[theY][theX].blue;
-        //                 } else {
-        //                     //this px doesnt exist, so subtract from tot (so that our average is accurate)
-        //                     tot--;
-        //                 }
-        //             }
-        //         }
-        //         //now average
-        //         avg.r = avg.r / tot;
-        //         avg.g = avg.g / tot;
-        //         avg.b = avg.b / tot;
-        //         //and push into chunkarr
-        //         chunkArr.push(avg);
-        //     }
-
-        // }
-        // return false;
-        vc(['chunk Arr length', chunkArr.length], vb)
-        //we now have an array of 'chunks'. For each chunk, get the color that corresponds with it
-        let str = '';
-        for (let n = 0; n < chunkArr.length; n++) {
-            if (!isNaN(chunkArr[n].r) && !isNaN(chunkArr[n].g) && !isNaN(chunkArr[n].b)) {
-                //if chunkArr exists at specified N, and its r,g, and b vals exist
-                let hsl = rgbToHsl(chunkArr[n].r, chunkArr[n].g, chunkArr[n].b);
-                let blok = n % process.stdout.columns == 0 ? ' ' : ' ';
-                if (limitCols) {
-                    if (chunkArr[n].r < 30 && chunkArr[n].g < 30 && chunkArr[n].b < 30) {
-                        //black
-                        finalColArr.push('bgBlack')
-                    } else if (chunkArr[n].r > 230 && chunkArr[n].g > 230 && chunkArr[n].b > 230) {
-                        //white
-                        finalColArr.push('bgWhite')
-                    } else if (hsl[1] > 0.1 && (hsl[2] * 300) > 125) {
-                        //light color, so bgCol
-                        if ((hsl[0] * 255) < 30) {
-                            finalColArr.push('bgRed')
-                        } else if ((hsl[0] * 255) < 90) {
-                            finalColArr.push('bgYellow')
-                        } else if ((hsl[0] * 255) < 150) {
-                            finalColArr.push('bgGreen')
-                        } else if ((hsl[0] * 255) < 210) {
-                            finalColArr.push('bgCyan')
-                        } else if ((hsl[0] * 255) < 270) {
-                            finalColArr.push('bgBlue')
-                        } else {
-                            finalColArr.push('bgMagenta')
-                        }
-                    } else if (hsl[1] > .1) {
-                        //dark color so just col
-                        if ((hsl[0] * 255) < 30) {
-                            finalColArr.push('red')
-                        } else if ((hsl[0] * 255) < 90) {
-                            finalColArr.push('yellow')
-                        } else if ((hsl[0] * 255) < 150) {
-                            finalColArr.push('green')
-                        } else if ((hsl[0] * 255) < 210) {
-                            finalColArr.push('cyan')
-                        } else if ((hsl[0] * 255) < 270) {
-                            finalColArr.push('blue')
-                        } else {
-                            finalColArr.push('magenta')
-                        }
+                        // }else{
+                        chunkArr.push('BREAK');
+                        // }
                     } else {
-                        //default to grey
-                        finalColArr.push('grey');
+                        chunkArr.push(sumPx);
                     }
-                } else if (colMode == '256') {
-                    str += fullCol.bgColor.ansi256.rgb(parseInt(chunkArr[n].r), parseInt(chunkArr[n].g), parseInt(chunkArr[n].b)) + blok + fullCol.bgColor.close;
-                } else if (colMode == '16' || colMode == '16m') {
-                    str += fullCol.bgColor.ansi16m.rgb(parseInt(chunkArr[n].r), parseInt(chunkArr[n].g), parseInt(chunkArr[n].b)) + blok + fullCol.bgColor.close;
                 }
             }
-        }
-        if (limitCols) {
-            for (var q = 0; q < finalColArr.length; q++) {
-                theFn = chalk[finalColArr[q]];
-                var pxTxt = finalColArr[q].indexOf('bg') != -1 ? ' ' : '#';
-                str += theFn(pxTxt) + theFn(pxTxt);
+            chunkArr = chunkArr.filter(a => typeof a == 'string' || !!a.t)
+            vc(['chunk Arr length', chunkArr.length, 'bad pixels', badPxs], vb)
+            //we now have an array of 'chunks'. For each chunk, get the color that corresponds with it
+            for (let n = 0; n < chunkArr.length; n++) {
+                if (chunkArr[n]=='BREAK'){
+                    str+='\n'
+                }else if(chunkArr[n] && chunkArr[n].r && !isNaN(chunkArr[n].r) && !isNaN(chunkArr[n].g) && !isNaN(chunkArr[n].b)) {
+                    //if chunkArr exists at specified N, and its r,g, and b vals exist
+                    let hsl = rgbToHsl(chunkArr[n].r, chunkArr[n].g, chunkArr[n].b);
+                    let blok = ' ';
+                    // vc(['COL MODE:',colMode],vb)
+                    if (limitCols) {
+                        let col = 'black';
+                        if (chunkArr[n].r < 30 && chunkArr[n].g < 30 && chunkArr[n].b < 30) {
+                            //black
+                            // finalColArr.push('bgBlack')
+                            col = 'black';
+                        } else if (chunkArr[n].r > 230 && chunkArr[n].g > 230 && chunkArr[n].b > 230) {
+                            //white
+                            // finalColArr.push('bgWhite')
+                            col = 'bgWhite';
+                        } else if (hsl[1] > 0.1 && (hsl[2] * 300) > 125) {
+                            //light color, so bgCol
+                            if ((hsl[0] * 255) < 30) {
+                                col = 'bgRed';
+                            } else if ((hsl[0] * 255) < 90) {
+                                col = 'bgYellow';
+                            } else if ((hsl[0] * 255) < 150) {
+                                col = 'bgGreen';
+                            } else if ((hsl[0] * 255) < 210) {
+                                col = 'bgCyan';
+                            } else if ((hsl[0] * 255) < 270) {
+                                col = 'bgBlue';
+                            } else {
+                                col = 'bgMagenta';
+                            }
+                        } else if (hsl[1] > .1) {
+                            //dark color so just col
+                            if ((hsl[0] * 255) < 30) {
+                                col = 'red';
+                            } else if ((hsl[0] * 255) < 90) {
+                                col = 'yellow';
+                            } else if ((hsl[0] * 255) < 150) {
+                                col = 'green';
+                            } else if ((hsl[0] * 255) < 210) {
+                                col = 'cyan';
+                            } else if ((hsl[0] * 255) < 270) {
+                                col = 'blue';
+                            } else {
+                                col = 'magenta';
+                            }
+                        } else {
+                            //default to grey
+                            col = 'grey';
+                        }
+                        str+=chalk[col](' ')
+                    } else if (colMode == '256') {
+                        str += fullCol.bgColor.ansi256.rgb(parseInt(chunkArr[n].r), parseInt(chunkArr[n].g), parseInt(chunkArr[n].b)) + blok + fullCol.bgColor.close;
+                    } else if (colMode == '16' || colMode == '16m') {
+                        str += fullCol.bgColor.ansi16m.rgb(parseInt(chunkArr[n].r), parseInt(chunkArr[n].g), parseInt(chunkArr[n].b)) + blok + fullCol.bgColor.close;
+                    }
+                }
+                // if ((n + 1) % process.stdout.columns == 0 && str[str.length - 1] != '|') {
+                //     str += '|'
+                // }
+                // if(!!bufferLen && (n)%dims.width===0){
+                //     //if this image is less wide than our terminal, append with strings
+                //     str+=' '.repeat(bufferLen)
+                // }
             }
-        }
+            // if (limitCols) {
+            //     for (var q = 0; q < finalColArr.length; q++) {
+            //         theFn = chalk[finalColArr[q]];
+            //         var pxTxt = finalColArr[q].indexOf('bg') != -1 ? ' ' : '#';
+            //         str += theFn(pxTxt) + theFn(pxTxt);
+            //     }
+            // }
+       
         if (exportMe) {
             def.resolve(str);
         } else {
+            // console.log("WOULD DRAW IMG HERE")
             console.log(str);
         }
     });
